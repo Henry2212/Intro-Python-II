6e8430e340cbd2c3419d909c4ce7101887b467ed
Henry <henrybsms@outlook.com>
2020-06-03 19:17:48 -0400

    day 1 mvp

--- src/room.py
+++ src/room.py
@@ -1,2 +1,19 @@
 # Implement a class to hold room information. This should have name and
-# description attributes.
+# description attributes.from item import Item
+from typing import List
+
+class Room:
+   class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.w_to = None
+        self.e_to = None
+
+    def get_name(self):
+        return f"{self.name}"
+
+    def get_description(self):
+        return f"{self.description}"
